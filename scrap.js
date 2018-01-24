ac430604e779ed5fb62af50f23b41f94fa0b082e
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-01-24 22:18:52 +0530

    Update scrap.js

--- scrap.js
+++ scrap.js
@@ -2,25 +2,18 @@ var request = require('request'),
     cheerio = require('cheerio'),
     fs = require('fs'),
 
-
     images = [];
 var rl = require('readline');
 var prompts = rl.createInterface(process.stdin, process.stdout);
 prompts.question("Enter Image Name : ", function(name) {
     var msg = " ";
-
-    request('https://alpha.wallhaven.cc/search?q=cars&search_image=', function(err, resp, body) {
+   request('https://alpha.wallhaven.cc/search?q=' + name, function(err, resp, body) {
         if (!err && resp.statusCode === 200) {
             var $ = cheerio.load(body);
 
             $('img', '#thumbs').each(function() {
-
-
-
-
-                var img = $(this).attr('data-src');
-
-                images.push(img);
+              var img = $(this).attr('data-src');
+                       images.push(img);
             });
             console.log(images);
         }
@@ -28,6 +21,4 @@ prompts.question("Enter Image Name : ", function(name) {
             request(images[i]).pipe(fs.createWriteStream('images/grub' + '.jpg'));
         }
     });
-
-
 });
